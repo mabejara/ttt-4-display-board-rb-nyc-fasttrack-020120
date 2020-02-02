18431979c9fcab469fd44ed17df3ed0e80a13a90
Mario <mabejara@gmail.com>
2020-02-02 18:32:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,5 +9,5 @@ def display_board(the_board)
     puts " #{the_board[6]} "," #{the_board[7]} "," #{the_board[9]} "
 
 display_board(turn_1)
-turn_2 = ["X"," "," "," ","0"," "," "," "," "]
-display_board(turn_2)
+
+end 
